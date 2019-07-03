1ac7c5b670a0d5f8759d11f518883be6d2b61fb6
hmiinyu <hmiinyu@gmail.com>
2019-07-03 15:08:25 +0800

    feat(): add meituan web app

---
+++ react-meituan-app/src/features/home/router/index.js
@@ -0,0 +1,11 @@
+import HomePage from '@/features/home/containers'
+
+// 多布局时，此处可导出数组，指定每种布局对应的页面
+export default {
+  path: '',
+  name: '首页模块',
+  children: [
+    { name: '默认页', component: HomePage, default: true },
+    { name: '首页', path: '/home', component: HomePage }
+  ]
+}
